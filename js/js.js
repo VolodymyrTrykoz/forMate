b4f6460ffd8fcd41b46ee8f23370fdc5d766dff4
VolodymyrTrykoz <mr.mastachok@gmail.com>
2017-08-25 20:16:24 +0300

    final

--- js/js.js
+++ js/js.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
 	new WOW().init();
-	setTimeout(function(){
-		var preloader = $('#page-preloader');
-		if(!preloader.hasClass('done')) {
-			preloader.addClass('done');
-		}
-	}, 1000	);
-	
+
 	
 });//end ready
