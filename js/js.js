2ad4734b81841fe6bf65535af73d6d1abbd85f86
VolodymyrTrykoz <mr.mastachok@gmail.com>
2017-08-25 19:44:12 +0300

    preloader added 25082017

--- js/js.js
+++ js/js.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 	new WOW().init();
+	setTimeout(function(){
+		var preloader = $('#page-preloader');
+		if(!preloader.hasClass('done')) {
+			preloader.addClass('done');
+		}
+	}, 1000	);
 	
 	
 });//end ready
